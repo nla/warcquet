dd6cbe75d72cb1e8876032e68ddd321b2f525bbf
Alex Osborne <aosborne@nla.gov.au>
2022-09-28 15:56:38 +0900

    Replace fixed_len_byte_array with binary in schema
    It seems the Apache Spark parquet reader can't handle it. :(

--- src/org/netpreserve/warcquet/UpgradeSchema.java
+++ src/org/netpreserve/warcquet/UpgradeSchema.java
@@ -14,16 +14,17 @@ import java.io.IOException;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 import java.util.function.Consumer;
 
 public class UpgradeSchema {
     public static void main(String[] args) throws IOException {
-        try (var reader = new WarcquetReader(Paths.get(args[0]));
+        Path inFile = Paths.get(args[0]);
+        try (var reader = new WarcquetReader(inFile);
              var writer = Warcquet.newWriter(Paths.get(args[1]))) {
             for (CaptureEvent event: reader) {
-                System.out.println(event.getUrl());
                 writer.write(event);
             }
         }
